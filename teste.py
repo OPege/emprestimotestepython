c83b5bc3550d81a270af0340ab80931fb79a9b82
Alexia <lecaestudante@gmail.com>
2019-09-20 10:40:36 -0300

    alterar código

--- teste.py
+++ teste.py
@@ -1,7 +1,8 @@
 idade = int(input("Qual é sua idade ? \n"))
-if idade > 21 or idade < 65:
-	salario = float(input("Digite seu salário: \n"))
-	emprestimo = float(input("Qual valor do empréstimo? \n"))
+salario = float(input("Digite seu salário: \n"))
+emprestimo = float(input("Qual valor do empréstimo? \n"))
+
+if idade > 21 or idade < 65: 
 	if salario*10 > emprestimo: print("Empréstimo pré-aprovado!")
 	else: print("Salário incompatível pelo valor do empréstimo requerido")  
 else: print("Empréstimo negado por idade")
